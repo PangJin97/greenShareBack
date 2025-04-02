781f70e43b0338e2e67ca490f51c38b636ba3891
cjh1939 <chemddhkd@gmail.com>
2025-04-02 15:00:21 +0900

    백앤드 ê³로그인ã,회원가입

--- src/main/java/com/green/greenshare/User/UserController.java
+++ src/main/java/com/green/greenshare/User/UserController.java
@@ -1,5 +1,19 @@
 package com.green.greenshare.User;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+  private final UserService userService;
+
+  @PostMapping("")
+  public void insertUserList(@RequestBody UserDTO userDTO){
+    userService.insertUserList(userDTO);
+  }
+
+
 
 }
--- src/main/java/com/green/greenshare/User/UserDTO.java
+++ src/main/java/com/green/greenshare/User/UserDTO.java
@@ -4,9 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @ToString
 public class UserDTO {
+  private String userEmail;
+  private String userPassword;
+  private String userTel;
+  private String userRole;
+  private LocalDateTime joinData;
+  private String userName;
+
 
 }

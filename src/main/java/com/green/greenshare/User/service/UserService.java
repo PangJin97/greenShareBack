2cf0461e8b42196a22ed0e7253c98f22cf192ddf
alee5899 <alee5899@naver.com>
2025-04-11 12:20:44 +0900

    충돌 해결

--- src/main/java/com/green/greenshare/User/service/UserService.java
+++ src/main/java/com/green/greenshare/User/service/UserService.java
@@ -1,8 +1,23 @@
-package com.green.greenshare.User.service;
+package com.green.greenshare.user.service;
 
-import com.green.greenshare.User.dto.UserDTO;
+import com.green.greenshare.user.dto.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
-  public int insertUserList(UserDTO userDTO);
+  //qna 질문 목록 조회
+  public List<UserDTO> qnaList(UserDTO userDTO);
+
+  //qna 질문 상세 조회
+  public UserDTO selectQna(int qnaNum);
+
+  //qna 질문 등록
+  public int insertQna(UserDTO userDTO);
+
+  //qna 글 수정
+  public int updateQna(UserDTO userDTO);
+
+  //qna 글 삭제
+  public int deleteQna(int qnaNum);
 }

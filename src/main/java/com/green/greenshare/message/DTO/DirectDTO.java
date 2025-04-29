04e0399e9e4678858c3583e5118567f942494867
hankiljung <hankil920602@gmail.com>
2025-04-29 10:42:52 +0900

    메세지 수정

--- src/main/java/com/green/greenshare/message/DTO/DirectDTO.java
+++ src/main/java/com/green/greenshare/message/DTO/DirectDTO.java
@@ -4,7 +4,7 @@ import com.green.greenshare.user.dto.UserDTO;
 import lombok.Data;
 
 @Data
-public class DirectDTO {
+public class DirectDTO {  /* 쓰레드 DTO */
   private Long threadId;        // 메시지가 속한 스레드 ID
   private String sender;        // 보낸 사람
   private String receiver;      // 받는 사람
